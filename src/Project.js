107326e13b8e3e5bce0da959fb7f6a2e46599588
Henry Alpert <halpert3@gmail.com>
2019-07-05 15:17:23 -0500

    initial commit

---
+++ src/Project.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Project.css'
+
+class Project extends React.Component {
+    render() {
+        return (
+            <h1>Blegh!</h1>
+        );
+    }
+}
+export default Project;
