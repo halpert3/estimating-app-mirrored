db4a5a37a6f33d88fde2bfefeacf8ba6305da88f
Henry Alpert <halpert3@gmail.com>
2019-07-09 15:39:09 -0500

    tweaked grid and formatting

--- src/Project.js
+++ src/Project.js
@@ -4,18 +4,25 @@ import './Project.css'
 class Project extends React.Component {
     state = {
         name: "Henry",
-        rate: "$68.21",
-        estimated_hours: "30"
+        rate: 68.25,
+        estimated_hours: 30
     }
     render() {
         return (
             <div className="grid-container">
                 <div className="middle-col">
                     <h1>Quote Builder</h1>
+                    <div className="header-container">
+                        <div id="headers">Name</div>
+                        <div id="headers">Rate</div>
+                        <div id="headers">Estimated Hours</div>
+                        <div id="headers">Total</div>
+                    </div>
                     <div className="chart-container">
                         <div>{this.state.name}</div>
-                        <div>{this.state.rate}</div>
+                        <div>${this.state.rate}</div>
                         <div>{this.state.estimated_hours}</div>
+                        <div>{this.state.estimated_hours * this.state.rate}</div>
                     </div>
                     <p>Name 2</p>
                     <p><button type="button">Add Person</button></p>
