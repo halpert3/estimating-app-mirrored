94d670d35058a87c4786b7f1b20eec06cbaeb439
Henry Alpert <halpert3@gmail.com>
2019-07-16 11:47:17 -0500

    formatting table

--- src/Project.js
+++ src/Project.js
@@ -1,14 +1,52 @@
 import React from 'react';
 import './Project.css'
 
+
+const ColoredLine = ({ color }) => (
+    <hr
+        style={{
+            backgroundColor: color,
+            height: 2
+        }}
+    />
+);
+const ListEngineers = props => {
+    return (
+        <div style={{ paddingBottom: 10 }}>
+            <table className="table">
+                {props.everyone.map((person, index) => {
+                    return (
+                        <tr key={index}>
+                            <td>{person.name}</td>
+                            <td>{person.rate}</td>
+                            <td>{person.estimated_hours}</td>
+                            <td>tbd</td>
+                        </tr>
+
+                    )
+                })}
+            </table>
+        </div>
+    )
+}
+
+
+
+
 class Project extends React.Component {
+
     state = {
-        name: "Henry",
-        rate: 68.25,
-        estimated_hours: 30
+        name: "George",
+        allEngineers: [
+            { name: "Henry", rate: 68.25, estimated_hours: 30 },
+            { name: "Terri", rate: 60, estimated_hours: 32 }
+        ]
     }
+
+
     render() {
         return (
+
             <div className="grid-container">
                 <div className="middle-col">
                     <h1>Quote Builder</h1>
@@ -18,17 +56,22 @@ class Project extends React.Component {
                         <div id="headers">Estimated Hours</div>
                         <div id="headers">Total</div>
                     </div>
-                    <div className="chart-container">
-                        <div>{this.state.name}</div>
-                        <div>${this.state.rate}</div>
-                        <div>{this.state.estimated_hours}</div>
-                        <div>{this.state.estimated_hours * this.state.rate}</div>
-                    </div>
-                    <p>Name 2</p>
-                    <p><button type="button">Add Person</button></p>
+                    <ListEngineers everyone={this.state.allEngineers} />
+                    <div><button type="button">Add Person</button></div>
+                    <br /><br />
+                    <div><ColoredLine color="yellowgreen" /></div>
+
+                    <div>{this.state.name}</div>
+
                 </div>
-            </div >
+            </div>
+
+
+
         );
     }
 }
 export default Project;
+
+
+
